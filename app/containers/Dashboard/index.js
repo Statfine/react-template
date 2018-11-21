f309066e34da317c80e4041dc711a9731379db7b
shaojia <jia.shao@eastiming.com>
2018-11-21 16:51:54 +0800

    fix antd sider

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -30,11 +30,11 @@ const AppWrapper = styled(Layout)`
   padding-top: 50px;
 `;
 
-const RightNav = styled(Layout)`
+const RightLayout = styled(Layout)`
   padding: 24px;
 `;
 
-const LeftNav = styled(Sider)`
+const LeftSilder = styled(Sider)`
   width: 200;
   background: #fff;
   box-shadow: 0 0 7px 0 rgba(0, 0, 0, 0.12);
@@ -46,22 +46,101 @@ const LeftNav = styled(Sider)`
   margin-bottom: -20px;
   overflow-y: auto;
 `;
-const Wrapper = styled(Content)`
+const WrapperContent = styled(Content)`
   background: #fff;
   padding: 24;
   margin: 0;
   min-height: 280px;
 `;
 
+export const urlToList = (url) => {
+  const urllist = url.split('/').filter(i => i);
+  return urllist.map((urlItem, index) => `/${urllist.slice(0, index + 1).join('/')}`);
+}
+
+/**
+ *  type  单个或者数组 single list
+ *  name 标题
+ *  url 路径
+ *  icon 图标
+ */
+const URL = [ // eslint-disable-line
+  {
+    type: 'single',
+    name: 'Option 1',
+    url: '/dashboard/option',
+    icon: 'pie-chart',
+  },
+  {
+    type: 'list',
+    name: 'Option 2',
+    url: '/dashboard/part',
+    icon: 'user',
+    listUrl: [
+      {
+        name: 'one',
+        url: '/dashboard/part/one',
+      },
+      {
+        name: 'two',
+        url: '/dashboard/part/two',
+      },
+    ],
+  },
+  {
+    type: 'list',
+    name: 'Option 3',
+    url: '/dashboard/edit',
+    icon: 'user',
+    listUrl: [
+      {
+        name: 'one',
+        url: '/dashboard/edit/one',
+      },
+      {
+        name: 'two',
+        url: '/dashboard/edit/two',
+      },
+    ],
+  },
+  {
+    type: 'single',
+    name: 'Option 4',
+    url: '/dashboard/user',
+    icon: 'pie-chart',
+  },
+]
+
 /* eslint-disable react/prefer-stateless-function */
 export class Dashboard extends React.PureComponent {
-  // constructor(props) {
-  //   super(props);
-  // }
+  /**
+   *  openKey 页面初始值，Item点击跳转之后改变
+   *  collapsed 侧边栏的开启与关闭
+   */
+  state = {
+    openKey: [],
+    collapsed: false,
+  }
 
-  onJump = url => this.props.history.push(url);
+  // Menud 点击
+  onHandleSelect = (openKey) => this.setState({ openKey });
+
+  // ITEM点击
+  onJump = (url, openKey = []) => {
+    this.props.history.push(url);
+    this.setState({ openKey })
+  }
+
+  // Get the currently selected menu
+  getSelectedMenuKeys = pathname => urlToList(pathname);
 
   render() {
+    const {
+      location: { pathname },
+    } = this.props;
+    const { openKey, collapsed } = this.state;
+    const selectedKeys = this.getSelectedMenuKeys(pathname);
+    const props = collapsed ? {} : { openKeys: openKey.length === 0 ? selectedKeys : openKey };
     return (
       <AppWrapper>
         <Helmet>
@@ -69,44 +148,81 @@ export class Dashboard extends React.PureComponent {
         </Helmet>
         <Header />
         <Layout>
-          <LeftNav>
+          <LeftSilder
+            trigger={null}
+            collapsible
+            collapsed={collapsed} 
+          >
             <Menu
+              key="Menu"
               mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
+              selectedKeys={selectedKeys}
+              onOpenChange={this.onHandleSelect}
               style={{ height: '100%', borderRight: 0 }}
-            >
+              {...props}
+            > 
+              <Menu.Item key="/dashboard/option" onClick={() => this.onJump('/dashboard/option')}>
+                <Icon type="pie-chart" />
+                <span>Option 1</span>
+              </Menu.Item>
               <SubMenu
-                key="sub1"
-                title={<span><Icon type="user" />subnav 1</span>}
+                key="/dashboard/part"
+                title={<div style={{ display: 'flex', alignItems: 'center' }}><Icon type="setting" /><span>Option 2</span></div>}
               >
-                <Menu.Item key="1" onClick={() => this.onJump('/dashboard')}>dashboard</Menu.Item>
-                <Menu.Item key="2" onClick={() => this.onJump('/dashboard/one')}>one</Menu.Item>
-                <Menu.Item key="3" onClick={() => this.onJump('/dashboard/two')}>two</Menu.Item>
-                <Menu.Item key="4" onClick={() => this.onJump('/dashboard/three')}>404</Menu.Item>
+                <Menu.Item key="/dashboard/part/one" onClick={() => this.onJump('/dashboard/part/one')}>one</Menu.Item>
+                <Menu.Item key="/dashboard/part/two" onClick={() => this.onJump('/dashboard/part/two')}>two</Menu.Item>
+                <Menu.Item key="" onClick={() => this.onJump('/dashboard/part')}>404</Menu.Item>
               </SubMenu>
+              <SubMenu
+                key="/dashboard/edit"
+                title={<div style={{ display: 'flex', alignItems: 'center' }}><Icon type="setting" /><span>Option 3</span></div>}
+              >
+                <Menu.Item key="/dashboard/edit/one" onClick={() => this.onJump('/dashboard/edit/one')}>one</Menu.Item>
+                <Menu.Item key="/dashboard/edit/two" onClick={() => this.onJump('/dashboard/edit/two')}>two</Menu.Item>
+              </SubMenu>
+              <Menu.Item key="/dashboard/user" onClick={() => this.onJump('/dashboard/user')}>
+                <Icon type="pie-chart" />
+                <span>Option 4</span>
+              </Menu.Item>
             </Menu>
-          </LeftNav>
-          <RightNav>
-            <Wrapper>
+          </LeftSilder>
+          <RightLayout>
+            <WrapperContent>
+              
               <Switch>
                 <Route
                   exact
                   path={this.props.match.url}
-                  component={() => <div>home</div>}
+                  render={() => <Redirect to={`${this.props.match.path}/option`} />}
                 />
                 <Route
-                  path={`${this.props.match.path}/one`}
+                  path={`${this.props.match.path}/option`}
+                  component={() => <div>option</div>}
+                />
+                <Route
+                  path={`${this.props.match.path}/part/one`}
                   component={() => <div>One</div>}
                 />
                 <Route
-                  path={`${this.props.match.path}/two`}
+                  path={`${this.props.match.path}/part/two`}
                   component={() => <div>Two</div>}
+                />
+                <Route
+                  path={`${this.props.match.path}/edit/one`}
+                  component={() => <div>edit One</div>}
+                />
+                <Route
+                  path={`${this.props.match.path}/edit/two`}
+                  component={() => <div>edit Two</div>}
+                />
+                <Route
+                  path={`${this.props.match.path}/user`}
+                  component={() => <div>user</div>}
                 />
                 <Route render={() => <Redirect to="/404" />} />
               </Switch>
-            </Wrapper>
-          </RightNav>
+            </WrapperContent>
+          </RightLayout>
         </Layout>
       </AppWrapper>
     );
@@ -116,6 +232,7 @@ export class Dashboard extends React.PureComponent {
 Dashboard.propTypes = {
   match: PropTypes.object,
   history: PropTypes.object,
+  location: PropTypes.object,
 };
 
 const mapStateToProps = createStructuredSelector({
